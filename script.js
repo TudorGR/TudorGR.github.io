ec43007470b9fa8dd569365d71f6beb731b3595d
Tudor Gradinaru <126348970+TudorGR@users.noreply.github.com>
2023-03-27 23:14:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+$(document).ready(function(){
+    $(".awards").waypoint(function(){
+        $(".awards").addClass("animate__animated animate__fadeInUp animate__fadeIn");
+    }, {offset:"80%"})
+    $(".featured").waypoint(function(){
+        $(".featured").addClass("animate__animated animate__fadeInUp animate__fadeIn");
+    }, {offset:"80%"})
+    $(".best-seller").waypoint(function(){
+        $(".best-seller").addClass("animate__animated animate__fadeIn");
+    }, {offset:"80%"})
+    $(".quotep").waypoint(function(){
+        $(".quotep").addClass("animate__animated animate__fadeIn");
+    }, {offset:"80%"})
+})
